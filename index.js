96ab9aa11bebfa1e039e20e955e9a37dd4a42c08
gangwar22 <147224060+gangwar22@users.noreply.github.com>
2024-06-28 04:40:54 +0530

    ok

--- index.js
+++ index.js
@@ -15,13 +15,19 @@ app.use(cors({
 }));
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb+srv://rahulgangwar22:Pcs6QGJe8GOObB59@cluster0.sbksrzf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { 
-  dbName:"Online_Learning" 
-})
-  .then(() => {
-    console.log("MongoDB connected");
-  })
-  .catch(err => console.error("MongoDB connection error:", err));
+const mongoose = require('mongoose');
+
+const uri = 'mongodb+srv://rahulgangwar22:Pcs6QGJe8GOObB59@cluster0.sbksrzf.mongodb.net/Online_Learning?retryWrites=true&w=majority';
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
+});
+
 
 app.use('/api', createUserRouter);
 app.use('/api', loginUserRouter);
